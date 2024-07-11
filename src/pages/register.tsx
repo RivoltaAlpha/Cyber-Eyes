3c24300be2f729f23a8991e429a48c09301d4771
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-07-11 07:01:21 +0300

    complete-ish

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,20 +1,21 @@
 import { NavLink } from 'react-router-dom';
-import loginPic from '../assets/Mobile login-pana.png';
+// import RegPic from '../assets/reg.png';
+import RegPic from '../assets/Learning-cuate.png';
 
 export default function Register() {
     return (
-        <div className="min-h-screen bg-white flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-            <div className="flex flex-col lg:flex-row items-center max-w-4xl w-full  p-10 space-y-8 lg:space-y-0 lg:space-x-8">
-                <div className="lg:w-3/4 lg:mr-11 w-full">
-                    <img src={loginPic} alt="Register" className="w-full h-auto object-cover" />
+        <div className="min-h-screen bg-white flex items-center justify-center px-2 sm:px-6 lg:px-4">
+            <div className="flex flex-col lg:flex-row items-center max-w-8xl p-10 space-y-8 lg:space-y-0 lg:space-x-2">
+                <div className="lg:w-full lg:ml-20 ">
+                    <img src={RegPic} alt="Register" className=" h-[600px] object-cover" />
                 </div>
-                <div className="lg:w-1/2 w-full space-y-8">
+                <div className="lg:w-[900px] lg:ml-11 w-full space-y-8">
                     <div>
-                        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                        <h2 className="mt-6 text-center text-3xl font-bold text-gray-900">
                             Create your account
                         </h2>
                     </div>
-                    <form className="mt-8 space-y-6" action="#" method="POST">
+                    <form className="mt-8 lg:mr-20 space-y-6" action="#" method="POST">
                         <input type="hidden" name="remember" defaultValue="true" />
                         <div className="rounded-md shadow-sm space-y-4">
                             <div>
@@ -22,7 +23,7 @@ export default function Register() {
                                     name="firstName"
                                     type="text"
                                     required
-                                    className="appearance-none rounded relative w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                    className="appearance-none rounded relative w-[600px] px-3 py-3 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                                     placeholder="First Name"
                                 />
                             </div>
@@ -31,7 +32,7 @@ export default function Register() {
                                     name="lastName"
                                     type="text"
                                     required
-                                    className="appearance-none rounded relative w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                    className="appearance-none rounded relative w-full px-3 py-3 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                                     placeholder="Last Name"
                                 />
                             </div>
@@ -40,7 +41,7 @@ export default function Register() {
                                     name="phone"
                                     type="tel"
                                     required
-                                    className="appearance-none rounded relative w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                    className="appearance-none rounded relative w-full px-3 py-3 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                                     placeholder="Phone"
                                 />
                             </div>
@@ -50,7 +51,7 @@ export default function Register() {
                                     type="email"
                                     autoComplete="email"
                                     required
-                                    className="appearance-none rounded relative w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                    className="appearance-none rounded relative w-full px-3 py-3 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                                     placeholder="Email address"
                                 />
                             </div>
@@ -60,7 +61,7 @@ export default function Register() {
                                     type="password"
                                     autoComplete="current-password"
                                     required
-                                    className="appearance-none rounded relative w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                    className="appearance-none rounded relative w-full px-3 py-3 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                                     placeholder="Password"
                                 />
                             </div>
@@ -68,7 +69,7 @@ export default function Register() {
                         <div>
                             <button
                                 type="submit"
-                                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-black hover:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                className="group relative w-1/2 flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-yellow-500 hover:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                             >
                                 Register
                             </button>
